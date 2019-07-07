ad26f0ac54b5da6c861507e54521567b64da031d
lujoamel <lucas.amlivia@fing.edu.uy>
2019-07-07 17:52:02 -0300

    - Authentuicating Users - Edititng User Profile - Changing User Password - Tweet Component - Tweets Component

--- tweetr-app/src/main.js
+++ tweetr-app/src/main.js
@@ -5,11 +5,18 @@ import App from './App'
 import router from './router'
 import axios from 'axios'
 import VeeValidate from 'vee-validate'
+import moment from 'moment'
 
 Vue.config.productionTip = false
 
 window.axios = axios
 axios.defaults.baseURL = 'http://127.0.0.1:3333'
+
+Vue.use(VeeValidate)
+// Global filter to format dates with momentjs
+Vue.filter('timeAgo', date => moment(date).fromNow())
+Vue.filter('joined', date => moment(date).format('MMMM YYYY'))
+Vue.filter('dob', date => moment(date).format('MMMM Do YYYY'))
 
 /* eslint-disable no-new */
 new Vue({
@@ -19,4 +26,8 @@ new Vue({
   template: '<App/>'
 })
 
-Vue.use(VeeValidate)
+// Vue.use(VeeValidate)
+// // Global filter to format dates with momentjs
+// Vue.filter('timeAgo', date => moment(date).fromNow())
+// Vue.filter('joined', date => moment(date).format('MMMM YYYY'))
+// Vue.filter('dob', date => moment(date).format('MMMM Do YYYY'))
--- tweetr-app/src/router/index.js
+++ tweetr-app/src/router/index.js
@@ -2,11 +2,22 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import SignUpForm from '@/components/Auth/SignUpForm'
+import LogInForm from '@/components/Auth/LogInForm'
+import UserProfileSettings from '@/components/User/Settings/UserProfileSettings'
+import UserPasswordSettings from '@/components/User/Settings/UserPasswordSettings'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/settings/password',
+      component: UserPasswordSettings
+    },
+    {
+      path: '/settings/profile',
+      component: UserProfileSettings
+    },
     {
       path: '/',
       name: 'HelloWorld',
@@ -15,6 +26,10 @@ export default new Router({
     {
       path: '/signup',
       component: SignUpForm
+    },
+    {
+      path: '/login',
+      component: LogInForm
     }
   ]
 })

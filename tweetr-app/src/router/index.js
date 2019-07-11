40dce92b1c8586ee3c0bd9a92aad9e59668c64af
lujoamel <lucas.amlivia@fing.edu.uy>
2019-07-10 23:38:53 -0300

    Displaying a Single Tweet

--- tweetr-app/src/router/index.js
+++ tweetr-app/src/router/index.js
@@ -5,11 +5,17 @@ import LogInForm from '@/components/Auth/LogInForm'
 import UserProfileSettings from '@/components/User/Settings/UserProfileSettings'
 import UserPasswordSettings from '@/components/User/Settings/UserPasswordSettings'
 import Home from '@/components/Home'
+import SingleTweet from '@/components/Tweet/SingleTweet'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/:username/status/:id',
+      component: SingleTweet,
+      props: true
+    },
     {
       path: '/',
       component: Home
